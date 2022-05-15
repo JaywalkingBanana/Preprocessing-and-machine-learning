4405a6e5cf69045aefbb30b986271fde7e732679
JaywalkingBanana <62420073+JaywalkingBanana@users.noreply.github.com>
2022-05-15 12:49:14 +0200

    Update useful_functions.py

--- useful_functions.py
+++ useful_functions.py
@@ -1,3 +1,17 @@
 #display image
 from IPython.display import display, Image
 display(Image(filename='download.jpeg'))
+
+#get image to matrix
+import matplotlib.image as image
+data=image.imread('download.jpeg')
+print('The Shape of the image is:',data.shape)
+print('The image as array is:')
+print(data)
+
+#matrix to image
+from PIL import Image
+import numpy as np
+img = Image.fromarray(data, 'RGB')
+img.save('my.png')
+img.show()
